55431e1d7f6eb79b0916081985f0206674828216
NKompa <nkompa7@gmail.com>
2022-08-22 18:23:38 +0300

    Example 3 Find in Array

---
+++ Example03_FindArray/Program.cs
@@ -0,0 +1,15 @@
+﻿// В массиве array с количеством элементов n нужно найти элемент find и указать его индекс. Find вводится произвольно.
+int[] array = { 178, 20, 34, 45, 82, 65, 26, 73, 82 };
+int n = array.Length;
+int find = 26;
+int index = 0;
+
+while(index <n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
